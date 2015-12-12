060183ed4aa3d5db439f62dd9eb62128860a69cc
Nic La <sakeofmaking@gmail.com>
2015-12-12 11:57:55 -0800

    Create heartbeat.h

---
+++ heartbeat.h
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * File Name:	heartbeat.h
+ * Program:		"Serial Echo" program for Fundamentals of Embedded Systems
+ * Author:		Robert Weber
+ * Purpose:		Contains heartbeat definition
+ *
+ *  Date	Changed by:	Changes:
+ * -------	-----------	-------------------------------------------------------
+ * 24Apr02	R Weber		Original file.
+ * 12Feb04	T Lill		Modified for class project.
+ ******************************************************************************/
+#if !defined(HEARTBEAT_H)		/* Prevents including this file multiple times */
+#define HEARTBEAT_H
+
+/* Function Prototypes */
+void heartbeat(void);
+
+#endif
