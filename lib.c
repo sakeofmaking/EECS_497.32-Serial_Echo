fdf30caa6b4c8ecd8e05cb440b4e98198856d0ea
Nic La <sakeofmaking@gmail.com>
2015-12-12 11:59:56 -0800

    Create lib.c

---
+++ lib.c
@@ -0,0 +1,13 @@
+/******************************************************************************
+ * File Name:	Lib.c
+ * Program:		"Serial Echo" program for Fundamentals of Embedded Systems
+ * Author:		Robert Weber
+ * Purpose:		Contains library-like functions.
+ *
+ *  Date	Changed by:	Changes:
+ * -------	-----------	-------------------------------------------------------
+ * 21Jul02	R Weber	Initial file
+ * 12Feb04	T Lill		Modified for class project.
+ ******************************************************************************/
+#include "lib.h"
+
